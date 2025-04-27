cdbc396f07a39bd4ab79ebae2d3afba04b13682f
Markololo <markololo2468@gmail.com>
2025-04-27 11:19:31 -0400

    Added some classes and code for multiple windows(staff and client windows)

--- ProgrammingPatternsProject/ProgrammingPatternsProject/src/main/java/org/example/GUIcontrollerStaff.java
+++ ProgrammingPatternsProject/ProgrammingPatternsProject/src/main/java/org/example/GUIcontrollerStaff.java
@@ -1,21 +1,31 @@
 package org.example;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class GUIcontroller {
+import java.io.IOException;
+import java.util.Objects;
+
+public class GUIcontrollerStaff {
+
     @FXML
     private Label welcomeLabel;
     @FXML
     private ComboBox<String> languageComboBox;
+    @FXML
+    private Button clientLoginBtn;
+    @FXML
+    private Button staffLoginBtn;
 
     private String selectedLanguage = "english"; //Default
     ClientController clientController;
     StaffController staffController;
 
-    public GUIcontroller() {
+    public GUIcontrollerStaff() {
         clientController = new ClientController();
         staffController = new StaffController();
     }
@@ -24,7 +34,33 @@ public class GUIcontroller {
     public void initialize() {
         languageComboBox.getItems().addAll("English", "French");
         languageComboBox.setValue("English"); //default
-        System.out.println("Empty window initialized.");
+        System.out.println("Empty staff window initialized.");
+    }
+
+    @FXML
+    public void handleStaffLoginButton() throws IOException {
+        //1) close previous window:
+        Stage prevStage = (Stage) staffLoginBtn.getScene().getWindow();
+        prevStage.close();
+        //2) open new window for the client:
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/MainView.fxml")));
+        primaryStage.setTitle("Staff Page");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    @FXML
+    public void handleClientLoginButton() throws IOException {
+        //1) close previous window:
+        Stage prevStage = (Stage) clientLoginBtn.getScene().getWindow();
+        prevStage.close();
+        //2) open new window for the client:
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ClientWindow.fxml")));
+        primaryStage.setTitle("Client Page");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     /**
