6da51fdfaa40523d1c34ab07743239ea34b54f4e
Markololo <markololo2468@gmail.com>
2025-04-27 01:13:40 -0400

    Added Language Handler File Using Factory Pattern

---
+++ ProgrammingPatternsProject/ProgrammingPatternsProject/src/main/java/org/example/MessageService.java
@@ -0,0 +1,45 @@
+package org.example;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+interface Language {
+    public String getMessage(String msgCategory);
+}
+
+//Concrete Classes
+class FrenchLang implements Language{
+    Locale francelocale=Locale.of("fr","FR");
+
+    public String getMessage(String msgCategory) {
+        ResourceBundle messages = ResourceBundle.getBundle("messages", francelocale);
+        return messages.getString(msgCategory);
+    }
+}
+
+class EnglishLang implements Language{
+    Locale uslocale=Locale.of("en","US");
+
+    public String getMessage(String msgCategory) {
+        ResourceBundle messages = ResourceBundle.getBundle("messages", uslocale);
+        return messages.getString(msgCategory);
+    }
+}
+
+
+class MessageFactory {
+    public static Language returnLang(String type) {
+        if (type.equalsIgnoreCase("french"))
+            return new FrenchLang();
+        else if (type.equalsIgnoreCase("english"))
+            return new EnglishLang();
+        throw new IllegalArgumentException("Unknown Language Type: " + type);
+    }
+}
+
+public class MessageService{
+    public String useLangService(String type, String msgCategory) {
+        Language lang = MessageFactory.returnLang(type);
+        return lang.getMessage(msgCategory);
+    }
+}
+
